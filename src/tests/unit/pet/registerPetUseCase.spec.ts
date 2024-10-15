a2bf75a07fbf3da5afa2b4f0e7f77541a42392ad
Alex Marques Beraldo <alexmarquesberaldo@gmail.com>
2024-10-15 20:04:16 -0300

    refactor: database refactor, removed model adoption needs, now its a column in model Pet

--- src/tests/unit/pet/registerPetUseCase.spec.ts
+++ src/tests/unit/pet/registerPetUseCase.spec.ts
@@ -12,26 +12,26 @@ let orgRepository: OrgRepository;
 let repository: PetRepository;
 
 describe("registerPetUseCase unit test", async () => {
-	beforeEach(async () => {
-		repository = new PetInMemoryRepository();
+    beforeEach(async () => {
+        repository = new PetInMemoryRepository();
         orgRepository = new OrgInMemoryRepository();
-		sut = new RegisterPetUseCase(repository, orgRepository);
-	});
+        sut = new RegisterPetUseCase(repository, orgRepository);
+    });
 
-	it("Should be able to register a new Pet", async () => {
+    it("Should be able to register a new Pet", async () => {
         const createOrgResponse = await orgRepository.registerOrg({
-			id: "1",
-			cnpj: "98.436.729/0001-00",
-			responsable_name: "Test Org",
-			email: "testorg@example.com",
-			password: "securepassword",
-			whatsapp: "1234567890",
-			cep: "22461-040",
-			city: "Testville",
-			street: "123 Test St",
-			neighborhood: "Test Neighborhood",
-			number: "123",
-		})
+            id: "1",
+            cnpj: "98.436.729/0001-00",
+            responsable_name: "Test Org",
+            email: "testorg@example.com",
+            password: "securepassword",
+            whatsapp: "1234567890",
+            cep: "22461-040",
+            city: "Testville",
+            street: "123 Test St",
+            neighborhood: "Test Neighborhood",
+            number: "123",
+        })
 
         const pet: Pet = {
             id: "1",
@@ -42,15 +42,16 @@ describe("registerPetUseCase unit test", async () => {
             energy_level: EnergyLevel.HIGH,
             name: "Buddy",
             org_id: createOrgResponse.id,
-            size: Size.LARGE
+            size: Size.LARGE,
+            adoption_requirements: "Requires a large yard and daily exercise"
         };
 
-		const response = await sut.execute(pet);
+        const response = await sut.execute(pet);
 
-		expect(response.pet).toEqual(
-			expect.objectContaining({ id: expect.any(String) })
-		);
-	});
+        expect(response.pet).toEqual(
+            expect.objectContaining({ id: expect.any(String) })
+        );
+    });
 
     it("Should not be able to register a pet without an existing org", async () => {
         const pet: Pet = {
@@ -62,7 +63,8 @@ describe("registerPetUseCase unit test", async () => {
             energy_level: EnergyLevel.HIGH,
             name: "Buddy",
             org_id: "1",
-            size: Size.LARGE
+            size: Size.LARGE,
+            adoption_requirements: "Requires a large yard and daily exercise"
         };
 
         await expect(sut.execute(pet)).rejects.toBeInstanceOf(ResourceNotFoundError);
